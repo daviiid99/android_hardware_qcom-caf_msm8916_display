a22b2b25a5b2fb686433c0653fac4428e22d405c
David <davidmolinamorales1999@gmail.com>
2021-08-30 08:56:07 +0200

    Update Android.mk

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -27,8 +27,8 @@ LOCAL_SHARED_LIBRARIES        += libqdutils libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := gpu.cpp gralloc.cpp framebuffer.cpp mapper.cpp
-LOCAL_COPY_HEADERS_TO         := $(common_header_export_path)
-LOCAL_COPY_HEADERS            := gralloc_priv.h gr.h
+LOCAL_EXPORT_C_INCLUDE_DIRS_TO         := $(common_header_export_path)
+LOCAL_EXPORT_C_INCLUDE_DIRS           := gralloc_priv.h gr.h
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -43,7 +43,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libdl
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdmemalloc\"
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := ionalloc.cpp alloc_controller.cpp
-LOCAL_COPY_HEADERS            := alloc_controller.h memalloc.h
+LOCAL_EXPORT_C_INCLUDE_DIRS            := alloc_controller.h memalloc.h
 
 ifeq ($(TARGET_USE_COMPAT_GRALLOC_ALIGN),true)
 LOCAL_CFLAGS += -DDISABLE_GET_PIXEL_ALIGNMENT
